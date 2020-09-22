343591200f4120c19d7cf743470536ccd51bb02f
Jaxon <mohamedmostels@gmail.com>
2020-09-22 23:33:16 +0200

    add CentOS 7 folder

--- centos-7/bootstrap.sh
+++ centos-7/bootstrap.sh
@@ -10,14 +10,14 @@ EOF
 
 # Install docker from Docker-ce repository
 echo "[TASK 2] Install docker container engine"
-yum install -y -q yum-utils device-mapper-persistent-data lvm2 vim > /dev/null 2>&1
-wget https://raw.githubusercontent.com/theJaxon/K8s-Vagrant/master/repo/docker-ce.repo -P /etc/yum.repos.d/
-yum repolist
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
 yum install -y -q docker-ce >/dev/null 2>&1
 
 # Enable docker service
 echo "[TASK 3] Enable and start docker service"
-systemctl enable --now docker >/dev/null 2>&1
+systemctl enable docker >/dev/null 2>&1
+systemctl start docker
 
 # Disable SELinux
 echo "[TASK 4] Disable SELinux"
