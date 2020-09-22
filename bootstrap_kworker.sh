f54b3b4d9fb81471149296841a592b9aecc9d7c9
Jaxon <mohamedmostels@gmail.com>
2020-09-22 23:25:48 +0200

    Upgrade to CentOS 8
    Switched from CentOS 7 to bento/CentOS 8
    - sshpass is now installed through epel-release
    - docker is installed through CentOS 8 repo (the default docker install instructions don't support CentOS 8 yet)

--- bootstrap_kworker.sh
+++ bootstrap_kworker.sh
@@ -2,6 +2,6 @@
 
 # Join worker nodes to the Kubernetes cluster
 echo "[TASK 1] Join node to Kubernetes Cluster"
-yum install -q -y sshpass >/dev/null 2>&1
+yum install -y epel-release && yum install -q -y sshpass 
 sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
 bash /joincluster.sh >/dev/null 2>&1
